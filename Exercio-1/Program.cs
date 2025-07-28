421cf994b48f0967b977d4f31182ee972b42a677
ValentimZx <nvalentim824@gmail.com>
2025-07-28 08:52:47 -0300

    Exercios

---
+++ Exercio-1/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.WriteLine("Exercio 1");
+
+Cliente cliente = new(); //CRIACAO DE UMA NOVA INSTANCIA
+
+Cliente c1 = new Cliente("Nicolas", "nvalentim824@gmail.com", 18);
+Cliente.ExibirInfo("Nicolas");
+Cliente.ExibirInfo("Nicolas", "nvalentim824@gmail.com");
+Cliente.ExibirInfo("Nicolas", "nvalentim824@gmail.com", 18);
+
+
+
+cliente.Nome = "Nícolas";
+cliente.Email = "nvalentim824@gmail.com";
+cliente.Idade = 18;
+
+Cliente.ExibirInfo("Nícolas", "nvalentim824@gmail.com", 18);
+Cliente.ExibirInfo("Nícolas", "nvalentim824@gmail.com");
+
+        Console.ReadKey();
+
+
+public struct Cliente //CRIANDO A STRUCT
+{
+    public string? Nome { get; set; } //PROPIEDADES GET E SET
+    public string? Email { get; set; }
+
+    private int idade;
+    public int Idade
+    {
+        get { return idade; }
+        set
+        {
+            if (value < 18)
+                idade = 18;
+        }
+    }
+
+    public Cliente(string nome, string email = "nvalentim824@gmail.com", int idade = 18) //CONSTRUTOR
+    {
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+
+    public static void ExibirInfo(string nome, string email = "nvalentim824@gmail.com", int idade = 18) //PARAMETRO OPCIONAL
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine($"Idade: {idade}");
+    }
+
+
+}
+
+
---
+++ Exercio-2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.Contracts;
+using CarroEnum;
+Console.WriteLine("=== Bem-Vindo ===");
+
+Carro c1 = new Carro();
+
+Console.WriteLine("Digite o modelo do carro: ");
+c1.Modelo = Console.ReadLine();
+
+Console.WriteLine("Digite a marca do carro: ");
+c1.Marca = Console.ReadLine();
+
+Console.WriteLine("Digite o ano do carro: ");
+c1.Ano = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a cor do carro: ");
+Console.WriteLine($"Branco: {(int)Cores.Branco}, Vermelho: {(int)Cores.Vermelho}, Preto: {(int)Cores.Preto}, Cinza: {(int)Cores.Cinza}, Prata: {(int)Cores.Prata}, Azul: {(int)Cores.Azul}");int valor = Convert.ToInt32(Console.ReadLine());
+var corEnum = (Cores)valor;
+c1.Cor = valor;
+c1.ExibirInfo(c1.Modelo, c1.Marca, c1.Ano, c1.Cor);
+
+Console.ReadKey();
+
+public class Carro
+{
+    public string? Modelo;
+    public string? Marca;
+    public int Ano;
+    public int Cor;
+
+    public void ExibirInfo(string? modelo, string marca, int ano, int cor = 1)
+    {
+        string? nomeCor = Enum.GetName(typeof(Cores), cor);
+
+        Console.WriteLine("\n=== Informações do carro ===");
+        Console.WriteLine($"Modelo: {modelo}");
+        Console.WriteLine($"Marca: {marca}");
+        Console.WriteLine($"Ano: {ano}");
+        Console.WriteLine($"Cor: {nomeCor ?? "Cor invalida!"}");
+    }
+
+    public Carro(string modelo, string marca, int ano, int cor)
+    {
+        Modelo = modelo;
+        Marca = marca;
+        Ano = ano;
+        Cor = cor;
+    }
+    public Carro()
+    {}
+ 
+}
